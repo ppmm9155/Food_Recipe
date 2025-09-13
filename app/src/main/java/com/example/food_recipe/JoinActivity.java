16d0f0e2f72135be50a9f691f7845c879b514440
jongsuha43-creator <jongsuha43@gmail.com>
2025-09-13 19:18:35 +0900

    종수 로그인 레이아웃 디자인 완료

---
+++ app/src/main/java/com/example/food_recipe/JoinActivity.java
@@ -0,0 +1,4 @@
+package com.example.food_recipe;
+
+public class JoinActivity {
+}
---
+++ app/src/main/java/com/example/food_recipe/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.food_recipe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText login_etEmail, login_etPassword;
+    private Button login_btnLogin;
+    private TextView login_tvJoint, login_tvFindId, login_tvFindPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        login_btnLogin = (Button) findViewById(R.id.login_btn);
+        login_etEmail = (EditText) findViewById(R.id.ETemail);
+        login_etPassword = (EditText) findViewById(R.id.ETpassword);
+        login_tvJoint = (TextView) findViewById(R.id.joinT);
+
+        // 로그인 버튼 클릭 시 동작 생성
+        login_btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = login_etEmail.getText().toString().trim();
+                String password = login_etPassword.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "이메일과 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                loginUser(email, password);
+            }
+        });
+
+        login_tvJoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    };
+}
